50acd35d39be61b4692c1c7f176383a6ff1525a0
GaryZhou98 <garyzhou98@gmail.com>
2025-02-13 21:49:50 -0800

    Create component to display transcription of user's audio
    - Per title
      - THe transcription box has a black bg with white fonts for readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/Header/Header";
+import TranscriptionBox from "@/components/TranscriptionBox/TranscriptionBox";
 
 export default function Home() {
   return (
     <div className="page-container">
       <Header />
       <main className="main-content">
-        {/* Your page content will go here */}
+        <TranscriptionBox />
       </main>
     </div>
   );
---
+++ src/components/TranscriptionBox/TranscriptionBox.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TranscriptionBox.css";
+
+const TranscriptionBox = () => {
+  return (
+    <div className="transcription-box">
+      <div className="transcription-content">
+        Your transcription will appear here...
+      </div>
+    </div>
+  );
+};
+
+export default TranscriptionBox;
