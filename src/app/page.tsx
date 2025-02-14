fe0bda52fcd36328bd23bdf62aea080dd9bd0b28
GaryZhou98 <garyzhou98@gmail.com>
2025-02-13 21:46:31 -0800

    Add main home container component
    - Per title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
 import Header from "@/components/Header/Header";
+
 export default function Home() {
-  return <Header />;
+  return (
+    <div className="page-container">
+      <Header />
+      <main className="main-content">
+        {/* Your page content will go here */}
+      </main>
+    </div>
+  );
 }
