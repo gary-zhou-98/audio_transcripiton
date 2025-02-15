01356f040a49040f310be9330c2eeb39a3a0e920
GaryZhou98 <garyzhou98@gmail.com>
2025-02-14 19:38:21 -0800

    implement Deepgram Context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MicrophoneProvider } from "@/contexts/MicrophoneContext";
+import { DeepgramProvider } from "@/contexts/DeepgramContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MicrophoneProvider>{children}</MicrophoneProvider>
+        <DeepgramProvider>
+          <MicrophoneProvider>{children}</MicrophoneProvider>
+        </DeepgramProvider>
       </body>
     </html>
   );
