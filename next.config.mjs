8ce2b2b895f4971ab6b48478ae30a3ac2c2aa2ff
GaryZhou98 <garyzhou98@gmail.com>
2025-02-17 23:48:09 -0800

    Revert changes to implement transcoder

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
-          },
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
--- src/components/AudioDownloader/AudioDownloader.tsx
+++ src/components/AudioDownloader/AudioDownloader.tsx
@@ -1,74 +1,20 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./AudioDownloader.css";
 import { useMicrophone } from "@/contexts/MicrophoneContext";
-import { FFmpeg } from "@ffmpeg/ffmpeg";
-import { fetchFile, toBlobURL } from "@ffmpeg/util";
 
 const AudioDownloader = () => {
   const { isRecording, audioBlob } = useMicrophone();
   const [downloadUrl, setDownloadUrl] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const ffmpegRef = useRef(new FFmpeg());
 
   useEffect(() => {
-    const load = async () => {
-      const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.6/dist/umd";
-      const ffmpeg = ffmpegRef.current;
-      ffmpeg.on("log", ({ message }) => {
-        console.log(message);
-      });
-      // toBlobURL is used to bypass CORS issue, urls with the same
-      // domain can be used directly.
-      await ffmpeg.load({
-        coreURL: await toBlobURL(
-          `${baseURL}/ffmpeg-core.js`,
-          "text/javascript"
-        ),
-        wasmURL: await toBlobURL(
-          `${baseURL}/ffmpeg-core.wasm`,
-          "application/wasm"
-        ),
-      });
-      setLoaded(true);
-    };
-
-    const transcode = async () => {
-      if (!audioBlob) {
-        return;
-      }
-      const ffmpeg = ffmpegRef.current;
-      const inputFileName = "input.webm";
-      const outputFileName = "output.mp3";
-      const blob = new Blob(audioBlob, { type: "audio/webm" });
-      await ffmpeg.writeFile(inputFileName, await fetchFile(blob));
-      await ffmpeg.exec([
-        "-i",
-        inputFileName,
-        "-vn", // Disable video
-        "-ar",
-        "44100", // Set audio sample rate to 44.1kHz
-        "-ac",
-        "2", // Set number of audio channels to 2
-        "-b:a",
-        "192k", // Set audio bitrate
-        outputFileName,
-      ]);
-      const data = await ffmpeg.readFile(outputFileName);
-      setDownloadUrl(
-        URL.createObjectURL(new Blob([data], { type: "video/mp4" }))
-      );
-    };
-
     if (audioBlob) {
-      if (!loaded) {
-        load();
-        return;
-      }
-      transcode();
+      setDownloadUrl(
+        URL.createObjectURL(new Blob(audioBlob, { type: "audio/webm" }))
+      );
     }
-  }, [audioBlob, loaded]);
+  }, [audioBlob]);
 
   if (isRecording) {
     return null;
